56e132b4894ef83aa7e96338171c6a5fead5f6bb
GonzaTJ <95258624+GonzaTJ@users.noreply.github.com>
2022-04-09 17:32:55 -0300

    Sonidos
    Se agrego los sonidos

--- js/motorcito.js
+++ js/motorcito.js
@@ -1,14 +1,30 @@
 var estadoLicuadora ="apagada";
 var licuadora = document.getElementById("blender");
+var sonidosLicuadora = document.getElementById("blender-sound");
+var botonLicuadora = document.getElementById("blender-button-sound");
 
+/* FUNCION DE APAGADO Y ENCENDIDO DE LA LICUADORA */
 function controlarLicuadora() {
   if (estadoLicuadora == "apagada"){
         estadoLicuadora = "encendido";
+        hacerBrrBrr();
         licuadora.classList.add("active");
-        console.log("me prendiste");
+        /* console.log("me prendiste"); */
     } else {
         estadoLicuadora = "apagada";
+        hacerBrrBrr();
         licuadora.classList.remove("active");
-        console.log("me apagaste");
+        /* console.log("me apagaste"); */
    }           
+}
+
+function hacerBrrBrr(){
+    if(sonidosLicuadora.paused){
+        botonLicuadora.play();
+        sonidosLicuadora.play();
+    } else {
+        botonLicuadora.play();
+        sonidosLicuadora.pause();
+        sonidosLicuadora.currentTime = 0;
+    }
 }
